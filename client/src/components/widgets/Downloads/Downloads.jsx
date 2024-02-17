288160d6e317cf9a0cfe401dc39e4fb5ef1549d9
pitersizovdev <pitersizov@gmail.com>
2024-02-17 19:51:37 +0300

    0.0.3

--- client/src/components/widgets/Downloads/Downloads.jsx
+++ client/src/components/widgets/Downloads/Downloads.jsx
@@ -4,7 +4,7 @@ import styles from './downloads.module.css'
 const Downloads = () => {
   return (
     <div className={styles.downloads}>
-        <img src='../../../../public/download/googleplay.svg' className={styles.googleplay}></img>
+        <img src={"../../../../public/download/googleplay.svg"}className={styles.googleplay}></img>
         <img src='../../../../public/download/appstore.svg' className={styles.ios}></img>
     </div>
   )
