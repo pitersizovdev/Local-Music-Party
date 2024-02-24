d184187d8ff00d8d4e90685c7b2c264e4e6dc7e7
pitersizovdev <pitersizov@gmail.com>
2024-02-24 19:27:31 +0300

    0.0.1

--- src/components/widgets/Header/Header.jsx
+++ src/components/widgets/Header/Header.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
-import styles from './header.module.css'
+import React from 'react';
+import styles from './header.module.css';
 import { ReactComponent as Vk } from '../../../assets/svg/vk.svg';
 import { ReactComponent as Tel } from '../../../assets/svg/tel.svg';
 import { ReactComponent as Dis } from '../../../assets/svg/dis.svg';
-
+import Lillogo3d from '../../../assets/img/lillogo3d.png';
 
 function Header() {
   return (
-    <div  className={styles.header}>
-      <h2>Local music party</h2>
-      <nav>
-        <Vk/>
-        <Tel/>
-        <Dis/>
+    <div className={styles.header}>
+      <h2 className={styles.title}>Local music party</h2>
+      <nav className={styles.nav}>
+        <Vk />
+        <Tel />
+        <Dis />
         <button>Скачать</button>
-        </nav>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header; 
+
