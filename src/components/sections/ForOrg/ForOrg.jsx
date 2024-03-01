62c4e49ccfcd84735c2ab352a86f76a272762a15
pitersizovdev <pitersizov@gmail.com>
2024-03-02 01:20:58 +0300

    0.3.0

--- src/components/sections/ForOrg/ForOrg.jsx
+++ src/components/sections/ForOrg/ForOrg.jsx
@@ -7,16 +7,16 @@ import Blob4 from '../../../assets/img/blob4.png';
 function ForOrg() {
   return (
     <div className={styles.section}>
-      <img className={styles.logobg} src={Logobg} alt="blob" />
+      <img className={styles.logobg} src={Logobg} alt="logo" />
       <div className={styles.wrapper}>
       <h1 className={styles.titleOrg}>Организаторам</h1>
       <h1 className={styles.titleArt}>и <br/>артистам</h1>
-        <img className={styles.mockupMap} src={MockupMap} alt="Logo" />
+        <img className={styles.mockupMap} src={MockupMap} alt="mockup" />
           <div className={styles.blob4}>
           <h3 className={styles.titleStep1}>Есть идея для <br/> мероприятия?</h3>
           <h3 className={styles.titleStep2}>Оставь заявку <br/> на нашем сайте</h3>
           <h3 className={styles.titleStep3}>Получи 10% баллами от <br/>ресторанов-партнеров</h3>
-          <img className={styles.blob4img} src={Blob4} alt="Logo" />
+          <img className={styles.blob4img} src={Blob4} alt="blob" />
         </div>
       </div>
     </div>
--- src/components/sections/ForUsers/ForUser.jsx
+++ src/components/sections/ForUsers/ForUser.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import styles from './forUsers.module.css'
+import MockupMap from '../../../assets/mockups/mockup.png';
+
 
 function ForUser() {
   return (
-    <div className={styles.section}>ForUser</div>
+    <div className={styles.section}>
+      <div className={styles.wrapper}>
+        <h1 className={styles.titleOrg}>Пользователям</h1>
+        <img className={styles.mockupMap} src={MockupMap} alt="mockup" />
+      </div>
+
+      <div className={styles.blob5}>
+        <h3 className={styles.titleStep1}>Узнайте о новых и набирающих <br/> популярность артистах</h3>
+        <h3 className={styles.titleStep2}>Сможете запланировать поход <br/> на мероприятие заранее</h3>
+        <h3 className={styles.titleStep3}>Посетите больше <br/> музыкальных мероприятиий!</h3>
+
+      </div>
+    </div>
   )
 }
 
