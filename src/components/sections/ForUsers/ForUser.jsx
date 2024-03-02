f291d659a119bda1df4033f2721ff53587cbc738
pitersizovdev <pitersizov@gmail.com>
2024-03-02 12:11:32 +0300

    0.4.0

--- src/components/sections/ForUsers/ForUser.jsx
+++ src/components/sections/ForUsers/ForUser.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import styles from './forUsers.module.css'
 import MockupMap from '../../../assets/mockups/mockup.png';
-
+import Blob5 from '../../../assets/img/blob5.png';
+import Blob6 from '../../../assets/img/blob6.png';
+import Blob7 from '../../../assets/img/blob7.png';
 
 function ForUser() {
   return (
@@ -13,9 +15,15 @@ function ForUser() {
 
       <div className={styles.blob5}>
         <h3 className={styles.titleStep1}>Узнайте о новых и набирающих <br/> популярность артистах</h3>
+        <img className={styles.blob5img} src={Blob5} alt="blob" />
+      </div>
+      <div className={styles.blob6}>
         <h3 className={styles.titleStep2}>Сможете запланировать поход <br/> на мероприятие заранее</h3>
-        <h3 className={styles.titleStep3}>Посетите больше <br/> музыкальных мероприятиий!</h3>
-
+        <img className={styles.blob6img} src={Blob6} alt="blob" />
+      </div>
+      <div className={styles.blob7}>
+        <h3 className={styles.titleStep3}>Посетите больше <br/> музыкальных <br/> мероприятиий!</h3>
+        <img className={styles.blob7img} src={Blob7} alt="blob" />
       </div>
     </div>
   )
