a6665e3cbbd62a666a736a6d29a46c893e41994d
pitersizovdev <pitersizov@gmail.com>
2024-03-02 21:26:30 +0300

    0.5.0

--- src/components/sections/Contact/Contact.jsx
+++ src/components/sections/Contact/Contact.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import styles from './contact.module.css'
 
-function Contact() {
+const Contact = () => {
   return (
-    <div className={styles.section}>Contact</div>
+    <div className={styles.section}>
+      <h1>Свяжитесь с нами</h1>
+      <div className={styles.wrapper}>
+      <form>
+
+        <input type="text" className={styles.leftcolumn}></input>
+
+        <div className={styles.rightcolumn}>
+          <input type="text"/>
+          <input type="email"/>
+          <select name="topic">
+            <option value="">Выберете тему обращения</option>
+            <option value="announcement">Заявка</option>
+            <option value="comment">Отзыв</option>
+            <option value="ask">Вопрос</option>
+          </select>
+          <button>Отправить</button>
+        </div>
+      </form>
+    </div>
+    </div>
   )
 }
 
