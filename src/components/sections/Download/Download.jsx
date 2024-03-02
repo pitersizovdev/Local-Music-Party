d34575d3c5453b0ab91b1d68928e2fa5ad6e3461
pitersizovdev <pitersizov@gmail.com>
2024-03-02 18:19:25 +0300

    0.4.2

--- src/components/sections/Download/Download.jsx
+++ src/components/sections/Download/Download.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import styles from './downl.module.css'
+import MiniLogo from '../../../assets/svg/minilogo.svg'
+import Qr from '../../../assets/svg/qr.svg'
+import Android from '../../../assets/img/android.png'
+import Ios from '../../../assets/img/ios.png'
 
 function Download() {
   return (
     <div className={styles.section}>
-      Download
-
+      <img className={styles.minilogo} src={MiniLogo} alt="logo" />
+      <img className={styles.qr} src={Qr} alt="qr" />
+      <img className={styles.android} src={Android} alt="android" />
+      <img className={styles.ios} src={Ios} alt="ios" />
     </div>
   )
 }
